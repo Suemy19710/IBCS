e238111c6ec5c05842ecf5cfe67717052e1d8e63
Suemy19710 <546682@student.fontys.nl>
2026-01-20 15:42:52 +0100

    update: minor changes

--- main.py
+++ main.py
@@ -28,12 +28,15 @@ app = FastAPI(title="IBCS Compliance API", version="1.0.0")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "https://ibcs-tau.vercel.app",
-        "http://localhost:8000", # 8000 for deployed backend 
-        "http://127.0.0.1:8000",
-        "http://localhost:3000", # when running localhost main.py
-        "http://127.0.0.1:3000",
-        "http://127.0.0.1:5500", # 5500 for frontend (not sure it works)
+            "http://127.0.0.1:5500",      # local frontend
+            "http://localhost:5500",      # if Live Server uses this
+            "https://ibcs-tau.vercel.app" # deployed frontend
+        # "https://ibcs-tau.vercel.app",
+        # "http://localhost:8000", # 8000 for deployed backend 
+        # "http://127.0.0.1:8000",
+        # "http://localhost:3000", # when running localhost main.py
+        # "http://127.0.0.1:3000",
+        # "http://127.0.0.1:5500", # 5500 for frontend (not sure it works)
 
     ],
     allow_credentials=True,
